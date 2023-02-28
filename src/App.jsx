9d4d15eeb3079307423e142116e191529cb00f32
Vianey <vianey@equipo3.com>
2023-02-28 15:15:11 -0600

    Inicializando Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  const name = "Vianey"
+
+  return (
+    <div>
+      <h3>LIstas de Hobbies {name}</h3>
+      <ol>
+        <li>Cocinar</li>
+        <li>Jugar</li>
+        <li>Bailar</li>
+      </ol>
+    </div>
+  )
+    
+}
+
+export default App
